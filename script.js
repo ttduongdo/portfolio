e785082c4fcc830b6e8c4fe426d69f53e022088e
ttduongdo <duongdo.38664@gmail.com>
2023-08-24 05:50:50 +0700

    changed section 1 2 3

--- script.js
+++ script.js
@@ -20,3 +20,42 @@ buttons.forEach(button => button.addEventListener('mouseout', () => {
     button.classList.remove('hovered');
     button.style.color = 'black';
 }))
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("slide");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  const projectLinks = document.querySelectorAll(".project");
+
+  projectLinks.forEach((link, index) => {
+    const projectURLs = [
+      "https://github.com/ttduongdo/rock-paper-scissor.git",
+      "https://github.com/ttduongdo/etch-a-sketch.git",
+      "https://github.com/ttduongdo/tic-tac-toe.git",
+      "https://github.com/ttduongdo/pizza-hangman.git",
+      "https://github.com/ttduongdo/pizza-hangman.git"
+    ];
+
+    link.href = projectURLs[index];
+  });
+});
+
+
+
+
+
