85b56160f52f4509986daa0b42804b101e327408
dttduong148 <ttdo@smith.edu>
2023-09-18 18:55:45 -0400

    1809 update

--- script.js
+++ script.js
@@ -15,35 +15,31 @@ showDivs(slideIndex += n);
 }
 
 function showDivs(n) {
-var i;
-var x = document.getElementsByClassName("slide");
-if (n > x.length) {slideIndex = 1}
-if (n < 1) {slideIndex = x.length} ;
-for (i = 0; i < x.length; i++) {
-x[i].style.display = "none";
-}
-x[slideIndex-1].style.display = "block";
+  var i;
+  var x = document.getElementsByClassName("slide");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+  x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
 }
 
 function goToLink(url) {
-window.location.href = url;
+  window.location.href = url;
 }
 
 function doGet(e) {
-var params = e.parameter;
-var name = params.name;
-var email = params.email;
-var message = params.message;
+  var params = e.parameter;
+  var name = params.name;
+  var email = params.email;
+  var message = params.message;
 
-var ss = SpreadsheetApp.openById('1uefnZeF3tCkLFdzoLSYiE3DtoWDXpg5AIgCWt6JpLRE');
-var sheet = ss.getSheetByName('port contact box'); // Change the sheet name if necessary
+  var ss = SpreadsheetApp.openById('1uefnZeF3tCkLFdzoLSYiE3DtoWDXpg5AIgCWt6JpLRE');
+  var sheet = ss.getSheetByName('port contact box'); // Change the sheet name if necessary
 
-sheet.appendRow([name, email, message]);
+  sheet.appendRow([name, email, message]);
 
-return ContentService.createTextOutput('Form submitted successfully.');
+  return ContentService.createTextOutput('Form submitted successfully.');
 }
-
-
-
-
 
